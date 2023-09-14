b837a7e7fd03bb145b68deab032fc4070f15136c
alphatechcamper <mizumoto2017@gmail.com>
2023-09-14 22:53:41 +0900

    ルーティングネスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :posts do
+   resource :likes, only: [:create, :destroy]
     collection do
       get 'search'
     end
